6911cac9d2f1784e528d0713aa010803bf8edaaf
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2017-12-15 18:14:31 +0700

    logger init with console and file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #ifndef NDEBUG
-#include <debug.h>
+#include <spdlog/spdlog.h>
 #endif
 #ifdef UNITTEST
 #include <gtest/gtest.h>
@@ -21,21 +21,55 @@ void display(L1List<VM_Record>& bList) {
 
 int main(int narg, char** argv) {
 #ifndef NDEBUG
+      // setup loggers
+      try {
+            // set async mode
+            spdlog::set_async_mode(8192);
+            // create two logger for console and file
+            auto console = spdlog::stdout_color_mt("console.log");
+            auto file    = spdlog::rotating_logger_mt(
+               "file.log", "logs/log.txt", 1024 * 1024 * 5, 3);
+
+            spdlog::drop_all();
+      } catch (const spdlog::spdlog_ex& ex) {
+            cout << "Some error occured\n";
+            return 255;
+      }
+      auto console = spdlog::get("console.log");
+      auto file    = spdlog::get("file.log");
       console->info("Logging system initialized");
+      file->info("Logging system initialized");
 #endif    // DEBUG
 #ifndef UNITTEST
       L1List<VM_Request> requestList;
       L1List<VM_Record>  db;
 
+#ifndef NDEBUG
+      console->info("Load request");
+      file->info("Load request");
+#endif
       loadRequests(argv[1], requestList);
+
+#ifndef NDEBUG
+      console->info("Load data");
+      file->info("Load data");
+#endif
       loadVMDB(argv[2], db);
 
       cout << fixed << setprecision(12);    // preset for floating point numbers
 
+#ifndef NDEBUG
+      console->info("Begin processing events");
+      file->info("Begin processing events");
+#endif
       process(requestList, db);
 
       cout << resetiosflags(ios::showbase) << setprecision(-1);
 
+#ifndef NDEBUG
+      console->info("All operation done");
+      file->info("All operation done");
+#endif
       return 0;
 #else    // UNITTEST
       testing::InitGoogleTest(&argc, argv);
