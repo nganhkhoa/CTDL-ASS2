7e87e02b051167b28a7e3a6c8989dfa1c7ba3763
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2017-12-16 08:51:23 +0700

    avl dessending insert test

--- test/AVL.cpp
+++ test/AVL.cpp
@@ -34,3 +34,38 @@ TEST(AVL, assending) {
       delete list;
       list = nullptr;
 }
+
+
+TEST(AVL, dessending) {
+      AVLTree<int> avl;
+      for (int i = 10; i < 0; i--)
+            avl.insert(i);
+      struct node
+      {
+            int data;
+            int height;
+      } sample[10] = {{7, 4},
+                      {3, 3},
+                      {2, 2},
+                      {1, 1},
+                      {5, 2},
+                      {4, 1},
+                      {6, 1},
+                      {9, 2},
+                      {8, 1},
+                      {10, 1}};
+
+      auto list = avl.getListNode();
+      list->traverse(
+         [](AVLNode<int>*& n, void* v) {
+               static int i      = 0;
+               node*      sample = (node*) v;
+               EXPECT_EQ(n->_data, sample[i].data);
+               EXPECT_EQ(n->_height, sample[i].height);
+               i++;
+         },
+         sample);
+
+      delete list;
+      list = nullptr;
+}
