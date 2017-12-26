d0c1ed0ec40d91870454e03aa56faaad52b3c66c
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2017-12-27 00:34:19 +0700

    fix using for to make it work when adding more test data

--- test/Request1.cpp
+++ test/Request1.cpp
@@ -5,6 +5,9 @@
 class req1_parsing : public ::testing::Test {
     protected:
       virtual void SetUp() {
+            for (auto& s : wrong_req_str)
+                  wrong_req.push_back(s);
+
             VM_Record dummy;
             dummy.timestamp = time(nullptr);
             List.insertHead(dummy);
@@ -15,6 +18,7 @@ class req1_parsing : public ::testing::Test {
 
       // empty data set
       L1List<VM_Record>        List;
+      std::vector<VM_Request>  wrong_req;
       std::vector<std::string> wrong_req_str{"1",
                                              "1_",
                                              "1_1",
@@ -24,26 +28,11 @@ class req1_parsing : public ::testing::Test {
                                              "1_1_1_111",
                                              "1_1_1_1111",
                                              "1_1_1_11111"};
-
-      VM_Request wrong_req[9]{
-         // init from string
-         wrong_req_str[0],
-         wrong_req_str[1],
-         wrong_req_str[2],
-         wrong_req_str[3],
-         wrong_req_str[4],
-         wrong_req_str[5],
-         wrong_req_str[6],
-         wrong_req_str[7],
-         wrong_req_str[8]
-         // one more is complete
-      };
 };
 
-
 TEST_F(req1_parsing, wrong_code) {
-      for (int i = 0; i < 9; ++i) {
-            auto ret = request1(wrong_req[0], List);
+      for (auto& req : wrong_req) {
+            auto ret = request1(req, List);
             ASSERT_EQ(returnType::type::boolean, ret.t);
             EXPECT_FALSE(ret.b);
       }
