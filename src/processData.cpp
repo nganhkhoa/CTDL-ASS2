314b213bf17c452208fe9fbc180c78a0ef18e85d
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2018-01-07 22:04:21 +0700

    fix alot and add request 5

--- src/processData.cpp
+++ src/processData.cpp
@@ -115,7 +115,7 @@ bool processRequest(
                   r = request4(request, recordList, *vehicles);
                   break;
             case 5:
-                  r = request5();
+                  r = request5(request, recordList);
                   break;
             case 6:
                   r = request6();
@@ -142,7 +142,7 @@ returnType request1(VM_Request& request, L1List<VM_Record>& list) {
       if (list.isEmpty())
             return {false};
 
-      auto thisTime = localtime(&list.at(0).timestamp);
+      auto thisTime = gmtime(&list.at(0).timestamp);
       if (thisTime == nullptr)
             return {"cannot get time from data"};
 
@@ -176,6 +176,7 @@ returnType request1(VM_Request& request, L1List<VM_Record>& list) {
                         strcmp(r.id, record_1.id) == 0 &&
                         r.timestamp == record_1.timestamp) {
                            record_1 = r;
+                           strcpy(record_1.id, "");
                      }
 
                      auto& record_2 = record->at(1);
@@ -183,34 +184,41 @@ returnType request1(VM_Request& request, L1List<VM_Record>& list) {
                         strcmp(r.id, record_2.id) == 0 &&
                         r.timestamp == record_2.timestamp) {
                            record_2 = r;
+                           strcpy(record_2.id, "");
                      }
+
+                     if (
+                        strcmp(record_1.id, "") == 0 &&
+                        strcmp(record_2.id, "") == 0)
+                           throw true;
                },
                &record);
       } catch (std::exception& e) {
+            // dummy exception
+      } catch (bool& b) {
+            // if both car is found
+            auto ret = new L1List<string>();
+
+            char RelativeLat = record.at(0).RelativeLatitudeTo(record.at(1));
+            char RelativeLon = record.at(0).RelativeLongitudeTo(record.at(1));
+
+            stringstream ss;
+            ss << fixed << setprecision(12)
+               << record.at(0).DistanceTo(record.at(1));
+
+            string distance = ss.str();
+            string lat      = to_string(RelativeLat);
+            string lon      = to_string(RelativeLon);
+
+            ret->push_back(lon);
+            ret->push_back(lat);
+            ret->push_back(distance);
+
+            return {ret};
       }
 
-      if (
-         record.at(0).longitude == 0 || record.at(0).latitude == 0 ||
-         record.at(1).longitude == 0 || record.at(1).latitude == 0)
-            return {false};
-
-      auto ret = new L1List<string>();
-
-      char RelativeLat = record.at(0).RelativeLatitudeTo(record.at(1));
-      char RelativeLon = record.at(0).RelativeLongitudeTo(record.at(1));
-
-      stringstream ss;
-      ss << fixed << setprecision(12) << record.at(0).DistanceTo(record.at(1));
-
-      string distance = ss.str();
-      string lat      = to_string(RelativeLat);
-      string lon      = to_string(RelativeLon);
-
-      ret->push_back(lon);
-      ret->push_back(lat);
-      ret->push_back(distance);
-
-      return {ret};
+      // when one car is not found
+      return {false};
 }
 returnType request2(
    VM_Request&        req,
@@ -234,22 +242,36 @@ returnType request2(
             AVLTree<string> out;
             double          longitude;
             char            direction;
+            size_t          num_vehicle;
       };
 
       Ans ans;
-      ans.longitude = req.params[1];
-      ans.direction = direction;
-      list.traverse(
-         [](VM_Record& vmr, void* v) {
-               Ans* ans = (Ans*) v;
-               if (vmr.RelativeLongitudeTo(ans->longitude) != ans->direction)
-                     return;
-               string id(vmr.id);
+      ans.longitude   = req.params[1];
+      ans.direction   = direction;
+      ans.num_vehicle = vehicles.getSize();
 
-               ans->out.insert(
-                  id, [](string& s1, string& s2) { return s1 == s2; });
-         },
-         &ans);
+      try {
+            list.traverse(
+               [](VM_Record& vmr, void* v) {
+                     Ans* ans = (Ans*) v;
+                     if (
+                        vmr.RelativeLongitudeTo(ans->longitude) !=
+                        ans->direction)
+                           return;
+                     string id(vmr.id);
+
+                     ans->out.insert(
+                        id, [](string& s1, string& s2) { return s1 == s2; });
+
+                     if (ans->num_vehicle == ans->out.getSize()) {
+                           throw true;
+                     }
+               },
+               &ans);
+      } catch (bool& b) {
+            // every vehicles went off track
+            return {(int) 0};
+      }
 
       return {(int) (vehicles.getSize() - ans.out.getSize())};
 }
@@ -275,22 +297,33 @@ returnType request3(
             AVLTree<string> out;
             double          latitude;
             char            direction;
+            size_t          num_vehicle;
       };
 
       Ans ans;
-      ans.latitude  = req.params[1];
-      ans.direction = direction;
-      list.traverse(
-         [](VM_Record& vmr, void* v) {
-               Ans* ans = (Ans*) v;
-               if (vmr.RelativeLatitudeTo(ans->latitude) != ans->direction)
-                     return;
+      ans.latitude    = req.params[1];
+      ans.direction   = direction;
+      ans.num_vehicle = vehicles.getSize();
 
-               string id(vmr.id);
-               ans->out.insert(
-                  id, [](string& s1, string& s2) { return s1 == s2; });
-         },
-         &ans);
+      try {
+            list.traverse(
+               [](VM_Record& vmr, void* v) {
+                     Ans* ans = (Ans*) v;
+                     if (
+                        vmr.RelativeLatitudeTo(ans->latitude) != ans->direction)
+                           return;
+
+                     string id(vmr.id);
+                     ans->out.insert(
+                        id, [](string& s1, string& s2) { return s1 == s2; });
+
+                     if (ans->num_vehicle == ans->out.getSize())
+                           throw true;
+               },
+               &ans);
+      } catch (bool& b) {
+            return {(int) 0};
+      }
 
       return {(int) (vehicles.getSize() - ans.out.getSize())};
 }
@@ -325,41 +358,89 @@ returnType request4(
       {
             VM_Request      req;
             AVLTree<string> list;
+            size_t          num_vehicle;
 
             Ans(VM_Request r) : req(r) {}
       };
+
       Ans ans(req);
+      ans.num_vehicle = vehicles.getSize();
 
-      list.traverse(
-         [](VM_Record& r, void* v) {
-               auto ans  = (Ans*) v;
-               auto hour = localtime(&r.timestamp)->tm_hour;
+      try {
+            list.traverse(
+               [](VM_Record& r, void* v) {
+                     auto ans  = (Ans*) v;
+                     auto hour = gmtime(&r.timestamp)->tm_hour;
 
-               // variables for easy reading
-               auto& lon    = ans->req.params[1];
-               auto& lat    = ans->req.params[2];
-               auto& radius = ans->req.params[3];
-               auto& start  = ans->req.params[4];
-               auto& end    = ans->req.params[5];
+                     // variables for easy reading
+                     auto& lon    = ans->req.params[1];
+                     auto& lat    = ans->req.params[2];
+                     auto& radius = ans->req.params[3];
+                     auto& start  = ans->req.params[4];
+                     auto& end    = ans->req.params[5];
 
-               if (start > hour || end < hour)
-                     return;
+                     if (start > hour || end < hour)
+                           return;
 
-               auto distance = r.DistanceTo(lat, lon);
+                     auto distance = r.DistanceTo(lat, lon);
 
-               if (distance > radius)
-                     return;
+                     if (distance > radius)
+                           return;
 
-               string id = r.id;
-               ans->list.insert(
-                  id, [](string& s1, string& s2) { return s1 == s2; });
-         },
-         &ans);
+                     string id = r.id;
+                     ans->list.insert(
+                        id, [](string& s1, string& s2) { return s1 == s2; });
+
+                     if (ans->num_vehicle == ans->list.getSize())
+                           throw true;
+               },
+               &ans);
+      } catch (bool& b) {
+            // if all vehicles is in the ring between that time
+            return {(int) vehicles.getSize()};
+      }
 
       return {(int) ans.list.getSize()};
 }
-returnType request5() {
-      return {false};
+returnType request5(VM_Request& req, L1List<VM_Record>& list) {
+      if (list.isEmpty())
+            return {(int) 0};
+
+      struct Ans
+      {
+            char   id[ID_MAX_LENGTH];
+            double lon;
+            double lat;
+            double radius;
+            int    occurence;
+      };
+
+      Ans ans;
+      int req_id;
+
+      if (
+         sscanf(
+            req.code,
+            "%1d_%16[A-Za-z0-9]_%10lf_%10lf_%10lf",
+            &req_id,
+            ans.id,
+            &ans.lon,
+            &ans.lat,
+            &ans.radius) != 5)
+            return {false};
+
+      list.traverse(
+         [](VM_Record& r, void* v) {
+               auto ans = (Ans*) v;
+               if (strcmp(ans->id, r.id) != 0)
+                     return;
+
+               if (r.DistanceTo(ans->lat, ans->lon) <= ans->radius)
+                     ans->occurence++;
+         },
+         &ans);
+
+      return {ans.occurence};
 }
 returnType request6() {
       return {false};
