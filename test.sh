5a74e7c5ed57a45fb3c2c89087f883479123b0e6
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2018-02-05 21:08:31 +0700

    more beautiful

--- test.sh
+++ test.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
-pwd='/home/luibo/Desktop/dsa171a2'
+pwd=$(pwd)  # get file path
 
 cowsay "Building...."
 cd $pwd/build
+make clean
 cmake .. -DCMAKE_BUILD_TYPE=Release -DUNIT_TEST=OFF 1> /dev/null
 make 1> /dev/null
+if [ $? -ne 0 ]; then
+      failed=1
+fi
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DUNIT_TEST=ON 2> /dev/null 1> /dev/null
 cd $pwd
 
-cowsay "The request file data: $(cat $pwd/resource/request.txt)"
+if [ $failed -eq 1 ]; then
+      cowsay -d "Sorry, build's Failed"
+      exit 1
+fi
+
+if [ $# -eq 2 ]; then
+      request=$1
+      data=$2
+elif [ $# -eq 1 ]; then
+      request=$1
+      data=$pwd'/resource/data.csv'
+else
+      request=$pwd'/resource/request.txt'
+      data=$pwd'/resource/data.csv'
+fi
+
+cowsay "The request file data: $(cat $request)"
 
 cowsay "Running my app...."
 my_start=$(date +%s)
-$pwd/bin/dsa171a2 $pwd/resource/request.txt $pwd/resource/data.csv > $pwd/testapp/my.log
+$pwd/bin/dsa171a2 $request $data > $pwd/testapp/my.log
 my_end=$(date +%s)
 
 cowsay "Running teacher's app...."
 teacher_start=$(date +%s)
-$pwd/testapp/dsa171a2 $pwd/resource/request.txt $pwd/resource/data.csv 2> /dev/null 1> $pwd/testapp/teacher.log
+$pwd/testapp/dsa171a2 $request $data 2> /dev/null 1> $pwd/testapp/teacher.log
 teacher_end=$(date +%s)
 
 cowsay "The result is:"
@@ -26,7 +46,7 @@ diff $pwd/testapp/my.log $pwd/testapp/teacher.log
 
 if [ $? -eq 0 ]; then
       echo "There's no difference, you are good to go"
-      cowsay "Good job"
+      cowsay -e "^^" "Good job"
 
       my_time=$(($my_end-$my_start))
       teacher_time=$(($teacher_end-$teacher_start))
